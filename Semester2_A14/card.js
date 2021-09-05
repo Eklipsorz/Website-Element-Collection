6109921b0539fd1341fefa56c41ba0423529ffb6
Eklipsorz <eklipsorz@protonmail.ch>
2021-09-05 17:39:13 +0800

    finish A14

--- Semester2_A14/card.js
+++ Semester2_A14/card.js
@@ -11,5 +11,35 @@ const profile = {
   
   // 由於 experiences 是陣列，最好先用迴圈把 <li></li> 的 template 整理好
   
+  function displayCard (profile) {
+    
+    let htmlContent = `
+                <img src=${profile['avatar']} alt="Avatar" class="rounded float-right pr-3" width="200">
+                <h3 class="my-name text-uppercase">${profile['name']}</h3>
+                <p class="info">${profile['info']}</p>
+                <div class="experience text-mute">
+                    <h6>Experience</h6>
+                    <ul class="list-unstyled small">
+    `
+
+    for (let experience of profile['experiences']) {
+
+        htmlContent += `
+                        <li>${experience}</li>
+        
+        `
+    }
+
+
+    htmlContent += `
+                    </ul>
+                </div>   
+    `
+
+
+    return htmlContent
+  }
   
   // 用 myCard.innerHTML 抽換內容
+
+  myCard.innerHTML = displayCard(profile)

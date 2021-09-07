47ec5c9a17a7aed3aaeb8bed0ca99f5d7c8abf31
Eklipsorz <eklipsorz@protonmail.ch>
2021-09-08 00:01:29 +0800

    fixed A3

--- Semester2_A3/function.js
+++ Semester2_A3/function.js
@@ -1,25 +1,27 @@
-function encodeName (name) {
-    // 請封裝你之前寫好的程式碼，並設計必要參數
-    return  name.substr(0,2) + '*'.repeat(name.length - 2)
+  // encodeName 功能為加密接收到的名字
+  // 參數說明： parameter1 是指要被加密的名字
+  function encodeName (name) {
+    // 以明碼顯示名字前兩個字元，後續字元全用*符號表示
+    return  name.substr(0, 2) + '*'.repeat(name.length - 2)
   }
-  
+    
+  // encodeEmail 功能為加密接收到的email
+  // 參數說明： parameter1 是指要被加密的email
   function encodeEmail (email) {
     // 請封裝你之前寫好的程式碼，並設計必要參數
-  
-  // find index of '@' sign and that index is just length of name part
-  let indexOfAtSign = email.indexOf('@') 
-  
-  // obtain half length of name part
-  let halfLengthOfUserName = Math.floor(indexOfAtSign / 2)
-  
-  // generate new emailUserName with '*'
-  let emailUserName = email.slice(0, halfLengthOfUserName) + '*'.repeat(indexOfAtSign - halfLengthOfUserName) 
-  
-  // get email suffix part
-  let emailSuffix = email.slice(indexOfAtSign, email.length)
-  
-  return emailUserName + emailSuffix
-  // combine new emailUserName with emailSuffix
+
+      // 找到@的索引值，而該值剛好是email使用者名稱的字元數
+      let indexOfAtSign = email.indexOf('@') 
+
+      // 獲取email使用者名稱的一半字元數。
+      let halfLengthOfUserName = Math.floor(indexOfAtSign / 2)
+
+      // 使用 "使用者名稱的一半字元以明碼顯示＋3個.符號＋@＋@後綴字" 來組成加密後的email
+      let econdedEmail = email.slice(0, halfLengthOfUserName) + 
+                        '.'.repeat(3)                        +
+                        email.slice(indexOfAtSign, email.length)
+
+      return econdedEmail
   }
   
   // 呼叫函式

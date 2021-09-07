41f5fab1ead7a610a248263ee26477c746edffeb
Eklipsorz <eklipsorz@protonmail.ch>
2021-09-07 23:59:31 +0800

    fixed A1

--- Semester2_A1/hideEmail.js
+++ Semester2_A1/hideEmail.js
@@ -1,19 +1,19 @@
 let email = 'bernard@example.com'
-// write your code
 
-// find index of '@' sign and that index is just length of name part
+
+// 請封裝你之前寫好的程式碼，並設計必要參數
+
+// 找到@的索引值，而該值剛好是email使用者名稱的字元數
 let indexOfAtSign = email.indexOf('@') 
 
-// obtain half length of name part
+// 獲取email使用者名稱的一半字元數。
 let halfLengthOfUserName = Math.floor(indexOfAtSign / 2)
 
-// generate new emailUserName with '*'
-let emailUserName = email.slice(0, halfLengthOfUserName) + '*'.repeat(indexOfAtSign - halfLengthOfUserName) 
+// 使用 "使用者名稱的一半字元以明碼顯示＋3個.符號＋@＋@後綴字" 來組成加密後的email
+let econdedEmail = email.slice(0, halfLengthOfUserName)     + 
+                   '.'.repeat(3)                            +
+                   email.slice(indexOfAtSign, email.length)
 
-// get email suffix part
-let emailSuffix = email.slice(indexOfAtSign, email.length)
-
-// combine new emailUserName with emailSuffix
-email = emailUserName + emailSuffix
-
+email = econdedEmail
+   
 console.log(email)
--- Semester2_A1/hideName.js
+++ Semester2_A1/hideName.js
@@ -1,8 +1,9 @@
 // input
 let name = 'Bernard'
-// write your code
 
-name = name.substr(0,2) + '*'.repeat(name.length - 2)
+// write your code
+// 先取出前兩個字元，剩下字元按照原本字元數來填入*
+name = name.substr(0, 2) + '*'.repeat(name.length - 2)
 
 // output
 console.log(name)

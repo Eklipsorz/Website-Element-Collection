55d2af4fb21a13b0309b0cc784236c1e96a8249b
Eklipsorz <eklipsorz@protonmail.ch>
2021-09-12 22:09:32 +0800

    finish A2

---
+++ Semester2-2_A2/womanAPI.js
@@ -0,0 +1,39 @@
+let button = document.querySelector('.btn')
+let dataPanel = document.querySelector('#data-panel')
+const url = 'https://randomuser.me/api/?gender=female&results=3'
+
+button.addEventListener('click', function (event) {
+
+  axios.get(url).then(function (response) {
+    // handle success
+    const womanSet = response.data.results
+
+    for (let woman of womanSet) {
+
+      // 使用desturing assignment按照woman物件所擁有的屬性取出來 
+      const { email, name, picture } = woman
+      // 建立存放名字、照片、電子郵件的框架 
+      const personItem = document.createElement('div')
+
+      personItem.classList.add('person-item')
+
+      personItem.innerHTML = `
+      <h3>${name.first + " " + name.last} </h3>
+      <img src="${picture.large}">
+      <p>${email}</p>
+      `
+
+      dataPanel.appendChild(personItem)
+    }
+    // console.log(userName)
+    // dataPanel.innerHTML = `
+    //   <h3>${userName.first + " " + userName.last} </h3>
+    //   <img src="${user.picture.large}">
+    //   <p>${user.email}</p>
+    //   `
+  }).catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+
+})
--- Semester2-2_U11/testAPI.js
+++ Semester2-2_U11/testAPI.js
@@ -4,15 +4,17 @@ let showLyricsList = document.querySelector('#showLyrics')
 
 
 button.addEventListener('click', function (event) {
-  axios.get('https://api.lyrics.ovh/v1/Coldplay/yellow')
+  let test = axios.get('https://api.lyrics.ovh/v1/Coldplay/yellow')
 
     .then(function (response) {
       // handle success
-      showLyricsList.innerHTML = response.data.lyrics
+      console.log(test)
+
+      //showLyricsList.innerHTML = response.data.lyrics
     })
     .catch(function (error) {
       // handle error
-      console.log(error);
+
     })
 
 

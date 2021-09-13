978757cc556f651ca899d4b43c4b6b664051cabd
Eklipsorz <eklipsorz@protonmail.ch>
2021-09-13 14:38:31 +0800

    another answer direction

--- Semester2-2_A3/AdelePlayList.js
+++ Semester2-2_A3/AdelePlayList.js
@@ -21,3 +21,52 @@ const album = {
 }
 
 // WRITE YOUR CODE ////////////////////////
+const activeItemIndex = 0
+const tracks = album.tracks
+
+/* 設定一開始預設被點擊的項目 */
+const activeItem = tracks[activeItemIndex]
+
+/* 從歌曲項目中移除預設被點擊的項目 */
+const inactiveTrackList = tracks.filter((track) => {
+  return track !== tracks[activeItemIndex]
+})
+
+/* 增加預設被點擊的項目至畫面中 */
+addSongList(activeItem, true)
+
+
+/* 增加剩餘項目至畫面中 */
+for (let track of inactiveTrackList) {
+  addSongList(track, false)
+}
+
+
+
+
+
+
+
+
+function addSongList(track, isActiveItem) {
+  /* 建立歌曲項目以及對應連結 */
+  const songListItem = document.createElement('li')
+  const songListItemLink = document.createElement('a')
+
+
+  /* 替項目、連結增加樣式 */
+  songListItem.classList.add('nav-item')
+  songListItemLink.classList.add('nav-link')
+
+  if (isActiveItem) {
+    songListItemLink.classList.add('active')
+  }
+
+  /* 設定連結內容並將連結本身設定給項目當作其內容 */
+  songListItemLink.innerHTML = track
+  songListItem.append(songListItemLink)
+
+  songList.append(songListItem)
+
+
+}

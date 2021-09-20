c99a924911be58d22eadae0555044f4770b1b217
Eklipsorz <eklipsorz@protonmail.ch>
2021-09-20 16:04:14 +0800

    fixed not found  in search bar

--- Semester2-2_MovieList/index.js
+++ Semester2-2_MovieList/index.js
@@ -185,7 +185,7 @@ function onSearchFormSubmitted(event) {
 
 
   if (!filteredMovies.length) {
-    alert('`您輸入的關鍵字：${keyword} 沒有符合條件的電影`')
+    alert(`您輸入的關鍵字：${keyword} 沒有符合條件的電影`)
   }
 
   // 渲染目前篩選的項目、分頁器

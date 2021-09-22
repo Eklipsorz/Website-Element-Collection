a965f3c4c3c32fd020455ab904598eef17e084b2
Eklipsorz <eklipsorz@protonmail.ch>
2021-09-23 00:24:55 +0800

    add gamefinished display

--- Semester2-2_card_game/main.js
+++ Semester2-2_card_game/main.js
@@ -113,6 +113,18 @@ const view = {
 
     })
 
+  },
+
+  showGameFinished() {
+    const div = document.createElement('div')
+    div.classList.add('completed')
+    div.innerHTML = `
+      <p>Complete!</p>
+      <p>Score: ${model.score}</p>
+      <p>You've tried: ${model.triedTimes} times</p>
+    `
+    const header = document.querySelector('#header')
+    header.before(div)
   }
 
 }
@@ -175,7 +187,13 @@ const controller = {
           view.pairCards(...model.revealedCards)
           model.revealedCards = []
 
+          if (model.score === 260) {
 
+            console.log('showGameFinished')
+            this.currentState = GAME_STATE.GameFinished
+            view.showGameFinished()  // 加在這裡
+            return
+          }
 
           this.currentState = GAME_STATE.FirstCardAwaits
 

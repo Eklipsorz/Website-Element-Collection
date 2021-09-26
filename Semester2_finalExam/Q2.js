128efcffa49645910b055b1d9a4b8aed73e7fd14
Eklipsorz <eklipsorz@protonmail.ch>
2021-09-26 21:12:42 +0800

    finish Q2.js

--- Semester2_finalExam/Q2.js
+++ Semester2_finalExam/Q2.js
@@ -2,13 +2,17 @@
 function findLongestWord(sentence) {
 
   // Step1: 將句子當中的單字找出來放到陣列當中
+
+  // 將字串轉換為陣列，其元素以字串的空格作為間隔來獲取
   const stringArray = sentence.split(' ')
   let longestWord = ''
+  // 定義最大長度為0，來方便尋找最長字串
   let maxLength = 0
   let indexOfLongestWord = 0
 
   // Step2: 找出長度最長的單字
 
+  // 從陣列中利用maxLength來尋找長度最長的字串，並從中取得對應的索引值
   for (let index = 0; index < stringArray.length; index++) {
 
     if (stringArray[index].length > maxLength) {
@@ -17,6 +21,7 @@ function findLongestWord(sentence) {
     }
   }
 
+  // 利用對應的索引值來找到最長字串，並回傳
   longestWord = stringArray[indexOfLongestWord]
 
   return longestWord
